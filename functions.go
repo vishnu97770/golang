d3b64119f45d4293ebc8afdde1457b95f5453211
vishnu97770 <vv162370@gmail.com>
2026-08-16 13:14:39 +0530

    code

--- functions.go
+++ functions.go
@@ -2,30 +2,10 @@ package main
 
 import "fmt"
 
-// simple function with parameters and a return value
-func add(a int, b int) int {
-	return a + b
+func greet() {
+    fmt.Println("Hello, Welcome to Go!")
 }
 
-// function with a single string return value
-func greet(name string) string {
-	return "Hello, " + name
-}
-
-// function returning multiple values
-func divide(a int, b int) (int, int) {
-	quotient := a / b
-	remainder := a % b
-	return quotient, remainder
-}
-
-func demoFunctions() {
-	sum := add(5, 3)
-	fmt.Println("Sum:", sum)
-
-	message := greet("Vishnu")
-	fmt.Println(message)
-
-	q, r := divide(17, 5)
-	fmt.Println("Quotient:", q, "Remainder:", r)
+func main() {
+    greet()
 }
